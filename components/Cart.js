9b897ee3e59cea93ee16f4ff1cf89ddf30717131
Sam Waller <samuellwaller@gmail.com>
2016-06-07 12:25:13 -0400

    trying to render 2nd component

---
+++ components/Cart.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+class Cart extends React.Component{
+  render(){
+    return (
+      <div>
+        <h1> Your Cart!
+          <ul>
+            <li></li>
+          </ul>
+        </h1>
+      </div>
+    );
+  }
+}
+
+module.exports = Cart
