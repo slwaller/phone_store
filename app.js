18ee14f1a297ea362daaaa3021fc61b4ba563b03
Sam Waller <samuellwaller@gmail.com>
2016-06-07 17:33:36 -0400

    changes

--- app.js
+++ app.js
@@ -6,23 +6,38 @@ require("./app.css");
 import Cart from './components/Cart'
 import Inventory from './components/Inventory'
 
-let thing = {
-  phones: [
-    { id: 1, name: "Sam's Busted Phone"},
-    { id: 2, name: "Nick's Beast"},
-    { id: 3, name: "Brian's Busted Ass Phone"}
-  ],
-  cartlist: []
-}
 
+  let thing = {
+    phones: [
+      { id: 1, name: "Sam's Busted Phone"},
+      { id: 2, name: "Nick's Beast"},
+      { id: 3, name: "Brian's Busted Ass Phone"}
+    ]
+  }
 
 class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = { cartlist: [] }
+  }
+
+  updateCartList(cartlist) {
+    this.setState({ cartlist: cartlist })
+  }
+
+  componentDidMount(){
+    this.setState({
+      cartlist: "TEST"
+    })
+  }
+
   render() {
     return (
       // React renders should only return 1 element
       <div>
         <Inventory phones={thing.phones}/>
-        <Cart list ={thing.cartlist}/>
+        <Cart list ={this.state.cartlist}/>
       </div>
     )
   }
